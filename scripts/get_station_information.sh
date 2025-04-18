9b98ae9f5ce0a6d78895ce348ec68b677e18cec4
David Duke <David.F.Duke@gmail.com>
2025-04-18 12:55:50 -0400

    Reworking script directories for json/csv files

--- scripts/get_station_information.sh
+++ scripts/get_station_information.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Downloading station information data for CitiBike..."
-wget -q https://gbfs.lyft.com/gbfs/2.3/bkn/en/station_information.json -O citibike_station_information.json
-echo "region_id, station_id, capacity, name, short_name, lon, lat, rental_uris.android, rental_uris.ios" > citibike_station_information.csv
-cat citibike_station_information.json | jq -r '.data.stations[] | [.region_id, .station_id, .capacity, .name, .short_name, .lon, .lat, .rental_uris.android, .rental_uris.ios] | @csv' >> citibike_station_information.csv
+wget -q https://gbfs.lyft.com/gbfs/2.3/bkn/en/station_information.json -O json_files/citibike_station_information.json
+echo "region_id, station_id, capacity, name, short_name, lon, lat, rental_uris.android, rental_uris.ios" > csv_files/citibike_station_information.csv
+cat json_files/citibike_station_information.json | jq -r '.data.stations[] | [.region_id, .station_id, .capacity, .name, .short_name, .lon, .lat, .rental_uris.android, .rental_uris.ios] | @csv' >> csv_files/citibike_station_information.csv
 
 echo "Downloading station information data for Divvy..."
-wget -q https://gbfs.lyft.com/gbfs/2.3/chi/en/station_information.json -O divvy_station_information.json
-echo "region_id, station_id, capacity, name, short_name, lon, lat, rental_uris.android, rental_uris.ios" > divvy_station_information.csv
-cat divvy_station_information.json | jq -r '.data.stations[] | [.region_id, .station_id, .capacity, .name, .short_name, .lon, .lat, .rental_uris.android, .rental_uris.ios] | @csv' >> divvy_station_information.csv
+wget -q https://gbfs.lyft.com/gbfs/2.3/chi/en/station_information.json -O json_files/divvy_station_information.json
+echo "region_id, station_id, capacity, name, short_name, lon, lat, rental_uris.android, rental_uris.ios" > csv_files/divvy_station_information.csv
+cat json_files/divvy_station_information.json | jq -r '.data.stations[] | [.region_id, .station_id, .capacity, .name, .short_name, .lon, .lat, .rental_uris.android, .rental_uris.ios] | @csv' >> csv_files/divvy_station_information.csv
--- scripts/get_system_regions.sh
+++ scripts/get_system_regions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Downloading system regions data for CitiBike..."
-wget -q https://gbfs.lyft.com/gbfs/2.3/bkn/en/system_regions.json -O citibike_system_regions.json
-echo "region_id, name" > citibike_system_regions.csv
-cat citibike_system_regions.json | jq -r '.data.regions[] | [.region_id, .name] | @csv' >> citibike_system_regions.csv
+wget -q https://gbfs.lyft.com/gbfs/2.3/bkn/en/system_regions.json -O json_files/citibike_system_regions.json
+echo "region_id, name" > csv_files/citibike_system_regions.csv
+cat json_files/citibike_system_regions.json | jq -r '.data.regions[] | [.region_id, .name] | @csv' >> csv_files/citibike_system_regions.csv
 
 echo "Downloading system regions data for Divvy..."
-wget -q https://gbfs.lyft.com/gbfs/2.3/chi/en/system_regions.json -O divvy_system_regions.json
-echo "region_id, name" > divvy_system_regions.csv
-cat divvy_system_regions.json | jq -r '.data.regions[] | [.region_id, .name] | @csv' >> divvy_system_regions.csv
+wget -q https://gbfs.lyft.com/gbfs/2.3/chi/en/system_regions.json -O json_files/divvy_system_regions.json
+echo "region_id, name" > csv_files/divvy_system_regions.csv
+cat json_files/divvy_system_regions.json | jq -r '.data.regions[] | [.region_id, .name] | @csv' >> csv_files/divvy_system_regions.csv
